e87f42b46affded08407058a4f62977a9d62e01f
coding <fingersfive@foxmail.com>
2016-07-26 10:29:33 +0800

    Initial Commit

---
+++ GDMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDMap
+//
+//  Created by Fingerfive on 16/7/26.
+//  Copyright © 2016年 Fingerfive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
